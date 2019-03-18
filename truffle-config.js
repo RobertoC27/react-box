6a12cc56e66c23145b8e16e57fb7fdcd7480faf2
RobertoC27 <chiroy_134@hotmail.com>
2019-03-18 09:46:12 -0600

    updated network and build folder configuration

--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,20 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  contracts_build_directory: "truffleContracts/",
+  networks: {
+    ganache: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9494,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
+    cloud: {
+      host: "10.1.1.1",     // Localhost (default: none)
+      port: 9494,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      websockets: true
+    },
+  }
 };

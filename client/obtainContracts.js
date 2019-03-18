0f60ba289ba85a53ff830c499f32d61d373cd5a6
RobertoC27 <chiroy_134@hotmail.com>
2019-03-18 09:55:41 -0600

    added script to extract contract info

---
+++ client/obtainContracts.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path')
+
+const contractsPath = path.resolve(__dirname, 'truffleContracts', 'contracts');
+const outPath = path.resolve(__dirname, 'src', 'utils', 'contractsInfo.js');
+
+// remove old file to for mental health
+try {
+    fs.unlinkSync(outPath);
+} catch (error) {
+    console.log('old file did not exist');
+}
+
+let contractContent, compiled, cPath;
+let contractInfo = {}
+let createdContracts = false;
+fs.readdirSync(contractsPath).forEach(file => {
+    if (!file.startsWith('Migrations.json')) {
+        cPath = path.resolve(contractsPath, file);
+        createdContracts = true;
+
+        contractContent = fs.readFileSync(cPath);
+        compiled = JSON.parse(contractContent);
+        contractInfo[file.substr(0, file.indexOf('.json'))] = { addr: compiled.networks[5777].address, abi: compiled.abi }
+    }
+})
+if (createdContracts) {
+    const varName = 'contractsInfo';
+    const result = `const ${varName} = ${JSON.stringify(contractInfo)} \nexport default ${varName}; \n`
+    fs.writeFileSync(outPath, result);
+}

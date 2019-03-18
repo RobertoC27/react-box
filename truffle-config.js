34612e2d05215421c72111ff01cfc580b619137c
RobertoC27 <chiroy_134@hotmail.com>
2019-03-18 09:51:43 -0600

    use path module instead of direct paths

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  contracts_build_directory: "truffleContracts/",
+  contracts_build_directory: path.resolve(__dirname, "truffleContracts/"),
   networks: {
     ganache: {
       host: "127.0.0.1",     // Localhost (default: none)
